a8b5fbff7b2204fcc617ce78e11eccef35cc4c90
Patrick Mennig <mail@patrick-mennig.de>
2018-12-03 19:14:26 +0100

    removed unneccessary code

--- src/createHtml.js
+++ src/createHtml.js
@@ -15,16 +15,3 @@ const createHtml = (workshopStore, templatePath) => {
   return result
 }
 exports.createHtml = createHtml
-
-
-
-
-// just for testing handlebars
-const compileTest = (data, templatePath) => {
-  const fs = require('fs')
-  const source = fs.readFileSync(templatePath, 'utf-8')
-  const template = handlebars.compile(source)
-  const result = template(data)
-  return result
-}
-exports.compileTest = compileTest
